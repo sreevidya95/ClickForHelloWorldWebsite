abb4216abbe6da3b55548847f9b00b60b7a79af8
sreevidya <sreevidya-bathalapa@pluralsight.com>
2024-05-03 21:08:18 +0530

    adding clear button and removing the text when clicked clear button

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,16 @@
 "use Sctrict";
 window.onload=init;
 function init(){
-    const helloBtn =document.getElementById("helloBtn");
+    let helloBtn =document.getElementById("helloBtn");
     helloBtn.onclick=onHelloBtnClicked;
+    const clearBtn =document.getElementById("clearBtn");
+    clearBtn.onclick=onClearBtnclicked;
 }
 function onHelloBtnClicked(){
    let message=document.getElementById('messageDiv');
     message.innerHTML="Hello World";
+}
+function onClearBtnclicked(){
+    let message = document.getElementById("messageDiv");
+    message.innerHTML="";
 }
